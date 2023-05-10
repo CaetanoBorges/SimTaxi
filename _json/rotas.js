63bb88ea0f6e175cf8ca9db815827ce2337047bb
cbcaetanoborges <cbcaetanoborges@gmail.com>
2023-05-10 15:18:21 +0100

    json

---
+++ _json/rotas.js
@@ -0,0 +1,17 @@
+[
+    {
+        "identificador":"ea986478j4azmnu",
+        "nome":"Rota Xistal",
+        "provincia":"Lubango",
+        "partida": [
+            "cordenate",
+            "Nome da partida"
+            ],
+        "destino": [
+            "cordenate",
+            "Nome da chegada"
+            ],
+        "preco": 98765
+    }
+]
+    
---
+++ _json/veiculos.js
@@ -0,0 +1,17 @@
+[
+    {
+        "identificador":"ea9864",
+        "motorista":"546564",
+        "fotos":[
+            "foto1.png",
+            "foto2.png",
+            "foto3.png"
+            ],
+        "tipo":"carro",
+        "matricula": "ABC-78-67",
+        "marca": "Toyota",
+        "modelo": "Hilux L200",
+        "cor":""
+    }
+]
+    

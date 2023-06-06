cdc906bbb58d4cd4e1f6ad42161c1e2598c8868a
cbcaetanoborges <cbcaetanoborges@gmail.com>
2023-06-06 16:06:33 +0100

    mensagem

---
+++ js/mapa.js
@@ -0,0 +1,27 @@
+function initMap(){
+            // The location of Uluru
+            const uluru = { lat: 55.0302, lng: 29.55 };
+            const ulur = { lat: 42.3601, lng: 71.0589 };
+            // The map, centered at Uluru
+            const map = new google.maps.Map(
+                document.getElementById("mapa-global"),
+                {
+                zoom: 4,
+                center: uluru,
+                }
+            );
+
+            function addMarker(coords){
+                const marker = new google.maps.Marker({
+                    position: coords,
+                    map: map
+                });
+            }
+
+            window.add=addMarker;
+            // The marker, positioned at Uluru
+            const mark = new google.maps.Marker({
+                position: uluru,
+                map: map,
+            });
+        }
